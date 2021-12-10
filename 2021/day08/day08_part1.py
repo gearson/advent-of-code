8efe8f67c1abc202bdcaa32f98b97f39199cabce
gearson <deniz.dohmen@gmail.com>
2021-12-10 12:19:08 +0100

    update

--- 2021/day08/day08_part1.py
+++ 2021/day08/day08_part1.py
@@ -2,24 +2,34 @@ from typing import List
 import pytest
 from aocd.models import Puzzle
 
-puzzle = Puzzle(year=2021, day=1)  # TODO: adjust day!
+puzzle = Puzzle(year=2021, day=8)  # TODO: adjust day!
 puzzle_input = puzzle.input_data
 
 
 def parse(puzzle_input: str) -> List:
     """parse input"""
-    data = [int(line) for line in puzzle_input.splitlines()]
+    data = [line for line in puzzle_input.splitlines()]
     return data
 
 
 def solve(data: List) -> int:
     """solve puzzle"""
-    return solution
+    pass
 
 
 INPUT_EXAMPLE = """\
+be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
 """
-OUTPUT_EXAMPLE = 5
+OUTPUT_EXAMPLE = 26
 
 
 @pytest.mark.parametrize(
